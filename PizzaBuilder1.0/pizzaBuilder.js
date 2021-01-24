58f91a1bbae992ad3788feec54ee11b820579cf5
Dzhem <cem_bekir@hotmail.com>
2021-01-24 14:18:25 +0200

    code refactor

--- PizzaBuilder1.0/pizzaBuilder.js
+++ PizzaBuilder1.0/pizzaBuilder.js
@@ -2,76 +2,59 @@ function init() {
 
     let totalPrice = 5;
 
-    let salamiCounter = 0;
-    let sausageCounter = 0;
-    let oliveCounter = 0;
-    let cheeseCounter = 0;
-    let cucumberCounter = 0;
-    let tomatoCounter = 0;
+    let salamiCounter = 0, sausageCounter = 0, oliveCounter = 0, cheeseCounter = 0, 
+    cucumberCounter = 0, tomatoCounter = 0;
 
-    const totalEl = document.getElementById('total');
+    const totalEl = document.querySelector('.total');
 
     function getRandomInt(min, max) {
         min = Math.ceil(min);
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-      }
+    }
+
+    totalPriceHandler = (productPrice) => {
+        totalPrice += productPrice;
+        totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+    }
+
+    addProductHandler = (productClass, productCounter, addClassName) => {
+
+        let el = document.querySelector(`.${productClass}`);
+        el.innerHTML = productCounter;
+
+        let newProduct = document.createElement('div');
+        newProduct.className = `${addClassName}`;
+        let top = getRandomInt(30, 500);
+        let left = getRandomInt(40, 500);
+        newProduct.style.top = `${top}px`;
+        newProduct.style.left = `${left}px`;
+        document.getElementById('pizza').appendChild(newProduct);
+    }
+
 
     addSalamiHandler = () => {
         if (salamiCounter < 10) {
             salamiCounter++;
 
-            let el = document.getElementById("1");
-            el.innerHTML = salamiCounter;
-
-            let newProduct = document.createElement('div');
-            newProduct.className = "addSalami";
-            let top = getRandomInt(30, 500);
-            let left = getRandomInt(40, 500);
-            newProduct.style.top = `${top}px`;
-            newProduct.style.left = `${left}px`;
-            document.getElementById('pizza').appendChild(newProduct);
-
-            totalPrice += 0.5;
-            totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+            addProductHandler('salami', salamiCounter, 'addSalami');
+            totalPriceHandler(0.5);
         }
     }
     addSausageHandler = () => {
         if (sausageCounter < 10) {
             sausageCounter++;
-            
-            let el = document.getElementById("2");
-            el.innerHTML = sausageCounter;
 
-            let newProduct = document.createElement('div');
-            newProduct.className = "addSausage";
-            let top = getRandomInt(30, 500);
-            let left = getRandomInt(40, 500);
-            newProduct.style.top = `${top}px`;
-            newProduct.style.left = `${left}px`;
-            document.getElementById('pizza').appendChild(newProduct);
-
-            totalPrice += 1;
-            totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+            addProductHandler('sausage', sausageCounter, 'addSausage');
+            totalPriceHandler(1);
         }
     }
     addOliveHandler = () => {
         if (oliveCounter < 10) {
             oliveCounter++;
 
-            let el = document.getElementById("3");
-            el.innerHTML = oliveCounter;
-
-            let newProduct = document.createElement('div');
-            newProduct.className = "addOlive";
-            let top = getRandomInt(30, 500);
-            let left = getRandomInt(40, 500);
-            newProduct.style.top = `${top}px`;
-            newProduct.style.left = `${left}px`;
-            document.getElementById('pizza').appendChild(newProduct);
-
-            totalPrice += 0.3;
-            totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+            addProductHandler('olive', oliveCounter, 'addOlive');
+            totalPriceHandler(0.3);
         }
 
     }
@@ -79,19 +62,8 @@ function init() {
         if (cheeseCounter < 10) {
             cheeseCounter++;
 
-            let el = document.getElementById("4");
-            el.innerHTML = cheeseCounter;
-
-            let newProduct = document.createElement('div');
-            newProduct.className = "addCheese";
-            let top = getRandomInt(30, 500);
-            let left = getRandomInt(40, 500);
-            newProduct.style.top = `${top}px`;
-            newProduct.style.left = `${left}px`;
-            document.getElementById('pizza').appendChild(newProduct);
-
-            totalPrice += 0.7;
-            totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+            addProductHandler('cheese', cheeseCounter, 'addCheese');
+            totalPriceHandler(0.7);
         }
 
     }
@@ -99,38 +71,16 @@ function init() {
         if (cucumberCounter < 10) {
             cucumberCounter++;
 
-            let el = document.getElementById("5");
-            el.innerHTML = cucumberCounter;
-
-            let newProduct = document.createElement('div');
-            newProduct.className = "addCucumber";
-            let top = getRandomInt(30, 500);
-            let left = getRandomInt(40, 500);
-            newProduct.style.top = `${top}px`;
-            newProduct.style.left = `${left}px`;
-            document.getElementById('pizza').appendChild(newProduct);
-
-            totalPrice += 0.1;
-            totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+            addProductHandler('cucumber', cucumberCounter, 'addCucumber');
+            totalPriceHandler(0.1);
         }
     }
     addTomatoHandler = () => {
         if (tomatoCounter < 10) {
             tomatoCounter++;
 
-            let el = document.getElementById("6");
-            el.innerHTML = tomatoCounter;
-
-            let newProduct = document.createElement('div');
-            newProduct.className = "addTomato";
-            let top = getRandomInt(30, 500);
-            let left = getRandomInt(40, 500);
-            newProduct.style.top = `${top}px`;
-            newProduct.style.left = `${left}px`;
-            document.getElementById('pizza').appendChild(newProduct);
-
-            totalPrice += 0.15;
-            totalEl.innerHTML = 'Total: $' + totalPrice.toFixed(2);
+            addProductHandler('tomato', tomatoCounter, 'addTomato');
+            totalPriceHandler(0.15);
         }
 
     }
